c040980fb5bce0b8c7d1f5ed6476d298bcfb744e
Ryan Du <ryandu343@gmail.com>
2025-06-26 12:19:31 -0400

    feat: Added a script to update the schedule data with the latest data and save it into a csv

---
+++ update/update_schedule.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import requests
+import re
+from datetime import datetime
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'utils'))
+from extract_schedule_data import extract_cebl_schedule
+from helpers import *
+
+def update_schedule_data():
+    year = datetime.now().year
+    schedule = pd.read_csv('cebl_schedule.csv')
+    current_schedule = extract_cebl_schedule(year)
+    current_schedule = current_schedule[~current_schedule['fiba_id'].isin(schedule['fiba_id'])]
+    current_schedule = current_schedule.query("status == 'COMPLETE'")
+    if not current_schedule.empty:
+        current_schedule = clean_schedule_data(current_schedule)
+
+    full_schedule = pd.concat([schedule, current_schedule], ignore_index=True)
+    full_schedule.to_csv('cebl_schedule.csv', index=False) # This is where I will save to releases
+
+update_schedule_data()
