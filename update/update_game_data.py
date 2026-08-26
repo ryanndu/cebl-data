e66b9a2e09f75112670fd3325306287ca398df25
David Awosoga <odo.awosoga@gmail.com>
2026-08-26 13:27:13 -0400

    update script to only write to releases if a new game is found

--- update/update_game_data.py
+++ update/update_game_data.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'utils'))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "utils"))
 from extract_schedule_data import extract_cebl_schedule
 from extract_game_data import *
 from helpers import *
@@ -22,32 +22,43 @@ def update_pbp_data():
     None
     """
     current_year = datetime.now().year
-    current_schedule = pd.read_csv('https://github.com/ryanndu/cebl-data/releases/download/schedule/cebl_schedule.csv')
+    current_schedule = pd.read_csv(
+        "https://github.com/ryanndu/cebl-data/releases/download/schedule/cebl_schedule.csv"
+    )
     current_schedule = current_schedule.query("season == @current_year")
     try:
-        pbp = pd.read_csv('https://github.com/ryanndu/cebl-data/releases/download/pbp/' + 'cebl_pbp_' + str(current_year) + '.csv')
-        current_schedule = current_schedule[~current_schedule['fiba_id'].isin(pbp['game_id'])]
+        pbp = pd.read_csv(
+            "https://github.com/ryanndu/cebl-data/releases/download/pbp/"
+            + "cebl_pbp_"
+            + str(current_year)
+            + ".csv"
+        )
+        current_schedule = current_schedule[
+            ~current_schedule["fiba_id"].isin(pbp["game_id"])
+        ]
     except Exception as e:
         pbp = pd.DataFrame()
 
     new_pbp = pd.DataFrame()
     for _, row in current_schedule.iterrows():
-        json_url = row['fiba_json_url']
-        game_id = re.search(r'/data/(\d+)/data\.json', json_url).group(1)
-        season = row['season']
+        json_url = row["fiba_json_url"]
+        game_id = re.search(r"/data/(\d+)/data\.json", json_url).group(1)
+        season = row["season"]
         try:
             json_data = requests.get(json_url).json()
-            json_data['game_id'] = game_id
-            json_data['season'] = season
-            new_pbp = pd.concat([new_pbp, extract_pbp_data(json_data)], ignore_index=True)
+            json_data["game_id"] = game_id
+            json_data["season"] = season
+            new_pbp = pd.concat(
+                [new_pbp, extract_pbp_data(json_data)], ignore_index=True
+            )
         except Exception as e:
             print(f"Error for game_id {game_id}: {e}")
             continue
     if not new_pbp.empty:
         new_pbp = clean_pbp_data(new_pbp)
-    all_pbp = pd.concat([pbp, new_pbp], ignore_index=True)
-    all_pbp.to_csv('cebl_pbp_' + str(current_year) + '.csv', index=False)
-    upload_to_releases('cebl_pbp_' + str(current_year) + '.csv', 'pbp')
+        all_pbp = pd.concat([pbp, new_pbp], ignore_index=True)
+        all_pbp.to_csv("cebl_pbp_" + str(current_year) + ".csv", index=False)
+        upload_to_releases("cebl_pbp_" + str(current_year) + ".csv", "pbp")
 
 
 def update_officials_data():
@@ -58,30 +69,38 @@ def update_officials_data():
     -------
     None
     """
-    officials = pd.read_csv('https://github.com/ryanndu/cebl-data/releases/download/officials/cebl_officials.csv')
+    officials = pd.read_csv(
+        "https://github.com/ryanndu/cebl-data/releases/download/officials/cebl_officials.csv"
+    )
     current_year = datetime.now().year
-    current_schedule = pd.read_csv('https://github.com/ryanndu/cebl-data/releases/download/schedule/cebl_schedule.csv')
+    current_schedule = pd.read_csv(
+        "https://github.com/ryanndu/cebl-data/releases/download/schedule/cebl_schedule.csv"
+    )
     current_schedule = current_schedule.query("season == @current_year")
-    current_schedule = current_schedule[~current_schedule['fiba_id'].isin(officials['game_id'])]
+    current_schedule = current_schedule[
+        ~current_schedule["fiba_id"].isin(officials["game_id"])
+    ]
 
     new_officials = pd.DataFrame()
     for _, row in current_schedule.iterrows():
-        json_url = row['fiba_json_url']
-        game_id = re.search(r'/data/(\d+)/data\.json', json_url).group(1)
-        season = row['season']
+        json_url = row["fiba_json_url"]
+        game_id = re.search(r"/data/(\d+)/data\.json", json_url).group(1)
+        season = row["season"]
         try:
             json_data = requests.get(json_url).json()
-            json_data['game_id'] = game_id
-            json_data['season'] = season
-            new_officials = pd.concat([new_officials, extract_officials_data(json_data)], ignore_index=True)
+            json_data["game_id"] = game_id
+            json_data["season"] = season
+            new_officials = pd.concat(
+                [new_officials, extract_officials_data(json_data)], ignore_index=True
+            )
         except Exception as e:
             print(f"Error for game_id {game_id}: {e}")
             continue
     if not new_officials.empty:
         new_officials = clean_officials_data(new_officials)
-    all_officials = pd.concat([officials, new_officials], ignore_index=True)
-    all_officials.to_csv('cebl_officials.csv', index=False)
-    upload_to_releases('cebl_officials.csv', 'officials')
+        all_officials = pd.concat([officials, new_officials], ignore_index=True)
+        all_officials.to_csv("cebl_officials.csv", index=False)
+        upload_to_releases("cebl_officials.csv", "officials")
 
 
 def update_coaches_data():
@@ -92,32 +111,40 @@ def update_coaches_data():
     -------
     None
     """
-    coaches = pd.read_csv('https://github.com/ryanndu/cebl-data/releases/download/coaches/cebl_coaches.csv')
+    coaches = pd.read_csv(
+        "https://github.com/ryanndu/cebl-data/releases/download/coaches/cebl_coaches.csv"
+    )
     current_year = datetime.now().year
-    current_schedule = pd.read_csv('https://github.com/ryanndu/cebl-data/releases/download/schedule/cebl_schedule.csv')
+    current_schedule = pd.read_csv(
+        "https://github.com/ryanndu/cebl-data/releases/download/schedule/cebl_schedule.csv"
+    )
     current_schedule = current_schedule.query("season == @current_year")
-    current_schedule = current_schedule[~current_schedule['fiba_id'].isin(coaches['game_id'])]
+    current_schedule = current_schedule[
+        ~current_schedule["fiba_id"].isin(coaches["game_id"])
+    ]
 
     new_coaches = pd.DataFrame()
     for _, row in current_schedule.iterrows():
-        json_url = row['fiba_json_url']
-        game_id = re.search(r'/data/(\d+)/data\.json', json_url).group(1)
-        season = row['season']
+        json_url = row["fiba_json_url"]
+        game_id = re.search(r"/data/(\d+)/data\.json", json_url).group(1)
+        season = row["season"]
         try:
             json_data = requests.get(json_url).json()
-            json_data['game_id'] = game_id
-            json_data['season'] = season
-            new_coaches = pd.concat([new_coaches, extract_coach_data(json_data)], ignore_index=True)
+            json_data["game_id"] = game_id
+            json_data["season"] = season
+            new_coaches = pd.concat(
+                [new_coaches, extract_coach_data(json_data)], ignore_index=True
+            )
         except Exception as e:
             print(f"Error for game_id {game_id}: {e}")
             continue
-    
+
     if not new_coaches.empty:
         new_coaches = clean_coach_data(new_coaches)
-    
-    all_coaches = pd.concat([coaches, new_coaches], ignore_index=True)
-    all_coaches.to_csv('cebl_coaches.csv', index=False)
-    upload_to_releases('cebl_coaches.csv', 'coaches')
+
+        all_coaches = pd.concat([coaches, new_coaches], ignore_index=True)
+        all_coaches.to_csv("cebl_coaches.csv", index=False)
+        upload_to_releases("cebl_coaches.csv", "coaches")
 
 
 def update_players_data():
@@ -128,33 +155,41 @@ def update_players_data():
     -------
     None
     """
-    players = pd.read_csv('https://github.com/ryanndu/cebl-data/releases/download/player-boxscore/cebl_players.csv')
+    players = pd.read_csv(
+        "https://github.com/ryanndu/cebl-data/releases/download/player-boxscore/cebl_players.csv"
+    )
     current_year = datetime.now().year
-    current_schedule = pd.read_csv('https://github.com/ryanndu/cebl-data/releases/download/schedule/cebl_schedule.csv')
+    current_schedule = pd.read_csv(
+        "https://github.com/ryanndu/cebl-data/releases/download/schedule/cebl_schedule.csv"
+    )
     current_schedule = current_schedule.query("season == @current_year")
-    current_schedule = current_schedule[~current_schedule['fiba_id'].isin(players['game_id'])]
+    current_schedule = current_schedule[
+        ~current_schedule["fiba_id"].isin(players["game_id"])
+    ]
 
     new_players = pd.DataFrame()
     for _, row in current_schedule.iterrows():
-        json_url = row['fiba_json_url']
-        game_id = re.search(r'/data/(\d+)/data\.json', json_url).group(1)
-        season = row['season']
+        json_url = row["fiba_json_url"]
+        game_id = re.search(r"/data/(\d+)/data\.json", json_url).group(1)
+        season = row["season"]
         try:
             json_data = requests.get(json_url).json()
-            json_data['game_id'] = game_id
-            json_data['season'] = season
-            new_players = pd.concat([new_players, extract_player_data(json_data)], ignore_index=True)
+            json_data["game_id"] = game_id
+            json_data["season"] = season
+            new_players = pd.concat(
+                [new_players, extract_player_data(json_data)], ignore_index=True
+            )
         except Exception as e:
             print(f"Error for game_id {game_id}: {e}")
             continue
 
     if not new_players.empty:
         new_players = clean_player_data(new_players)
-    
-    all_players = pd.concat([players, new_players], ignore_index=True)
-    all_players = clean_player_data(all_players)
-    all_players.to_csv('cebl_players.csv', index=False)
-    upload_to_releases('cebl_players.csv', 'player-boxscore')
+
+        all_players = pd.concat([players, new_players], ignore_index=True)
+        all_players = clean_player_data(all_players)
+        all_players.to_csv("cebl_players.csv", index=False)
+        upload_to_releases("cebl_players.csv", "player-boxscore")
 
 
 def update_team_data():
@@ -165,27 +200,35 @@ def update_team_data():
     -------
     None
     """
-    teams = pd.read_csv('https://github.com/ryanndu/cebl-data/releases/download/team-boxscore/cebl_teams.csv')
+    teams = pd.read_csv(
+        "https://github.com/ryanndu/cebl-data/releases/download/team-boxscore/cebl_teams.csv"
+    )
     current_year = datetime.now().year
-    current_schedule = pd.read_csv('https://github.com/ryanndu/cebl-data/releases/download/schedule/cebl_schedule.csv')
+    current_schedule = pd.read_csv(
+        "https://github.com/ryanndu/cebl-data/releases/download/schedule/cebl_schedule.csv"
+    )
     current_schedule = current_schedule.query("season == @current_year")
-    current_schedule = current_schedule[~current_schedule['fiba_id'].isin(teams['game_id'])]
+    current_schedule = current_schedule[
+        ~current_schedule["fiba_id"].isin(teams["game_id"])
+    ]
 
     new_teams = pd.DataFrame()
     for _, row in current_schedule.iterrows():
-        json_url = row['fiba_json_url']
-        game_id = re.search(r'/data/(\d+)/data\.json', json_url).group(1)
-        season = row['season']
+        json_url = row["fiba_json_url"]
+        game_id = re.search(r"/data/(\d+)/data\.json", json_url).group(1)
+        season = row["season"]
         try:
             json_data = requests.get(json_url).json()
-            json_data['game_id'] = game_id
-            json_data['season'] = season
-            new_teams = pd.concat([new_teams, extract_team_data(json_data)], ignore_index=True)
+            json_data["game_id"] = game_id
+            json_data["season"] = season
+            new_teams = pd.concat(
+                [new_teams, extract_team_data(json_data)], ignore_index=True
+            )
         except Exception as e:
             print(f"Error for game_id {game_id}: {e}")
             continue
     if not new_teams.empty:
         new_teams = clean_team_data(new_teams)
-    all_teams = pd.concat([teams, new_teams], ignore_index=True)
-    all_teams.to_csv('cebl_teams.csv', index=False)
-    upload_to_releases('cebl_teams.csv', 'team-boxscore')
+        all_teams = pd.concat([teams, new_teams], ignore_index=True)
+        all_teams.to_csv("cebl_teams.csv", index=False)
+        upload_to_releases("cebl_teams.csv", "team-boxscore")
--- update/update_schedule_data.py
+++ update/update_schedule_data.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'utils'))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "utils"))
 from extract_schedule_data import extract_cebl_schedule
 from helpers import *
 from upload_to_releases import upload_to_releases
@@ -17,14 +17,18 @@ def update_schedule_data():
     Updates the schedule data with new completed games for the current season.
     """
     year = datetime.now().year
-    schedule = pd.read_csv('https://github.com/ryanndu/cebl-data/releases/download/schedule/cebl_schedule.csv')
+    schedule = pd.read_csv(
+        "https://github.com/ryanndu/cebl-data/releases/download/schedule/cebl_schedule.csv"
+    )
     current_schedule = extract_cebl_schedule(year)
-    current_schedule = current_schedule[~current_schedule['fiba_id'].isin(schedule['fiba_id'])]
+    current_schedule = current_schedule[
+        ~current_schedule["fiba_id"].isin(schedule["fiba_id"])
+    ]
     current_schedule = current_schedule.query("status == 'COMPLETE'")
     if not current_schedule.empty:
         current_schedule = clean_schedule_data(current_schedule)
 
-    full_schedule = pd.concat([schedule, current_schedule], ignore_index=True)
-    full_schedule = clean_schedule_data(full_schedule)
-    full_schedule.to_csv('cebl_schedule.csv', index=False)
-    upload_to_releases('cebl_schedule.csv', 'schedule')
+        full_schedule = pd.concat([schedule, current_schedule], ignore_index=True)
+        full_schedule = clean_schedule_data(full_schedule)
+        full_schedule.to_csv("cebl_schedule.csv", index=False)
+        upload_to_releases("cebl_schedule.csv", "schedule")
